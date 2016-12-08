919b40570b123facb7ada237b4804876ef46b789
zeelock <10678@bwls.net>
2016-12-08 18:45:20 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,33 +1,55 @@
+var o=0;
 function setup() {
-  
-  createCanvas(1300,1200);
-  
-  
-  
-  
+
+createCanvas (windowWidth,windowHeight,WEBGL);
+
+}
+function draw() {
+ 
+ background(0);
+ 
+ 
+ 
+ push();
+ //ambientLight(0);
+ rotateX(frameCount*0.1);
+ pointLight(0, 100, 250, 0, 1, -10);
+ ambientMaterial(200);
+ pop();
+//translate( 0 , 0 , (mouseX-0.5*windowWidth)*1);
+//rotateX((mouseY-0.5*windowHeight)*0.01);
+ellipsoid(50,50,50);
+
+  moons( 4 , 200 , 50 , frameCount*0.09 , frameCount*0.01);
+      // 10 is too big and 9 lags it   (4 is the best)
+camera(2000);
+
 }
 
-function draw() {
+var moons = function( add , dis , die ,angx,angy ){
+ 
+         var xax;
+         for(xax = 0; xax < add; xax++ ){
   
-  fill(r=244,b=0,y=0);
-  ellipse( mouseX , mouseY ,50 , 50);
+   
   
-  fill(r=244,b=244,y=244);
-  ellipse( mouseX , mouseY ,40 , 40);
-  
-   fill(r=244,b=0,y=0);
-  ellipse( mouseX , mouseY ,30 , 30);
-  
-  fill(r=244,b=244,y=244);
-  ellipse( mouseX , mouseY ,20 , 20);
-  
-   fill(r=244,b=0,y=0);
-  ellipse( mouseX , mouseY ,10 , 10);
-  
-  fill(r=244,b=244,y=244);
-  ellipse( mouseX , mouseY ,1 , 1);
-  
-  
-  
-  
-}
+  push();
+        
+            rotateX(angx+xax/add*PI);
+            rotateY(angy+xax/add*PI);
+//            rotateX( angx + xax/add*2*PI);
+         
+           
+           translate ( 100 , 0 ,0 );
+            ellipsoid (die, die, die);
+          
+          
+          if (die > 10){
+           
+            moons ( add ,dis*0.7 , die-10 , angx*1.1 , angy*1.1 );
+           
+         
+          }
+          pop();
+         }
+};
